8dd0433fb66ccd9687eafef03857f9fa3ec4e7d4
Ornella Sereno Alloa <90015172+Ornella07@users.noreply.github.com>
2023-11-16 14:33:36 -0300

    Update ProductManager.js

--- src/ProductManager.js
+++ src/ProductManager.js
@@ -1,131 +1,83 @@
+import fs from "fs";
 
-const fs = require('fs');
-
-class ProductManager {
-    constructor(fileName){
-        this.fileName = fileName;
-        if(fs.existsSync(fileName)){
-            try{
-                let products = fs.readFileSync(fileName, 'utf-8');
-                this.products = JSON.parse(products)
-            }catch(error){
-                this.products = [];
-            }
-        }else{
-            this.products = [];
-            fs.writeFileSync(this.fileName, JSON.stringify(this.products), 'utf-8');
-        }
-    }
-    getProducts(){
-        return this.products;
-    }
-    async readFile(){
-        try{
-            this.products = JSON.parse(await fs.readFileSync(this.fileName, 'utf-8'))
-        }
-        catch(error){
-            console.error(`Error ${error}`);
-        }
-    }
-    async saveFile(){
-        try{
-            await fs.promises.writeFile(
-                this.fileName,
-                JSON.stringify(this.products,null, '\t'),'utf-8');
-        }catch(error){
-            console.error(`Error ${error}`);
-        }
-
+export default class ProductManager {
+    constructor(jsonFileName) {
+        this.jsonFileName = jsonFileName;
+        this.products = [];
+        this.id = 0;
     }
 
-    async addProducts(product){
-       await this.readFile();
-       const existeElCodigo = this.products.find((p) => p.code === product.code)
-       if(existeElCodigo){
-        console.log('Error, el codigo existe');
-       }else{
-        const newProduct = {...product, id:this.products.length + 1};
-        this.products.push(newProduct)
+    async init() {
+        try {
+            const data = await fs.readFile(this.jsonFileName, 'utf-8');
+            this.products = JSON.parse(data);
+            // ? Encontramos el último ID al inicializarse
+            this.id = this.products.reduce((maxId, product) => Math.max(maxId, product.id), 0);
+        } catch (error) {
+            // ! Si hay un error al leer el archivo, asumimos que es porque no existe o el mismo esta vacion, lo manejamos creando un nuevo archivo.
+            await this.saveFile();
+        }
+    }
+    async saveFile() {
+        await fs.writeFile(this.jsonFileName, JSON.stringify(this.products, null, 2), 'utf-8');
+    };
+
+    async addPorduct(title, description, price, thumbnail, code, stock) {
+        const newProduct = {
+            id: ++this.id, //! Incrementamos el último ID y asignamos al nuevo producto
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock,
+        };
+        //? Verificamos si ya existe un producto con el mismo codigo.
+        if (this.products.some(product => product.code === code)) {
+            console.error(`El producto con el código ${code} ya existe`);
+            return;
+        }
+        this.products.push(newProduct);
         await this.saveFile();
-       }
+    };
+
+    async getProducts(limit) {
+        //* Retornar productos limitados si se especifica un limite
+        return limit ? this.products.slice(0, limit) : this.products;
+    }
+
+    async getProductsById(productId) {
+        return this.products.find(product => producto.id === productId);
     }
 
 
-    async deleteProduct(id){
+
+    async deleteProduct(id) {
         const prodSelect = this.products.find((p) => p.id == id);
-        if(prodSelect){
-            const newProdArr = this.products.filter((p)=> p.id != id);
+        if (prodSelect) {
+            const newProdArr = this.products.filter((p) => p.id != id);
 
             this.products = newProdArr;
 
             await this.saveFile();
-        }else{
-            console.log('Error al eliminar el producto con id');
+        } else {
+            console.log("Error al eliminar el producto con id");
         }
     }
-    //aca se recibe el id 
-    async updateProductById ({id, ...newValuesForProduct}) {
-        await this.readFile();
-        
-  const productsForUpdate = this.products.find((p) => p.id === id);
-    if (productsForUpdate !== -1) {
-      this.products[productsForUpdate] = { ...this.products[productsForUpdate],newValuesForProduct };
-      this.saveFile(); // Guardar los productos en el archivo después de la actualización
-      return this.products[productsForUpdate];
-    } else {
-      console.error(`Producto con id: ${id} no encontrado`);
-    }
-  }
- 
- 
+    //aca se recibe el id
+    async updateProductById({ id, ...newValuesForProduct }) {
+        this.readFile();
 
-}
-
-class Products {
-    constructor(title, description, price, thumbnail, code, stock){
-        (this.title = title),
-        (this.description = description),
-        (this.price = price),
-        (this.thumbnail = thumbnail),
-        (this.code = code),
-        (this.stock = stock);
+        const productsForUpdate = this.products.find((p) => p.id === id);
+        if (productsForUpdate !== -1) {
+            this.products[productsForUpdate] = {
+                ...this.products[productsForUpdate],
+                newValuesForProduct,
+            };
+            await this.saveFile(); // Guardar los productos en el archivo después de la actualización
+            return this.products[productsForUpdate];
+        } else {
+            console.error(`Producto con id: ${id} no encontrado`);
+        }
     }
 }
-
-
-// (async () => {
-//     const Productos = new ProductManager('./productos.json');
-
-//    await Productos.addProducts(new Products('Producto 1', 'Descripción 1', 100, 'imagen1.jpg', 'code1', 10))
-//    await Productos.addProducts(new Products('Producto 2', 'Descripción 2', 100, 'imagen2.jpg', 'code2', 20))
-//    await Productos.addProducts(new Products('Producto 3', 'Descripción 3', 100, 'imagen3.jpg', 'code3', 30))
-//    await Productos.addProducts(new Products('Producto 4', 'Descripción 4', 100, 'imagen4.jpg', 'code4', 40))
-//    await Productos.addProducts(new Products('Producto 5', 'Descripción 5', 100, 'imagen5.jpg', 'code5', 50))
-//    await Productos.addProducts(new Products('Producto 6', 'Descripción 6', 100, 'imagen6.jpg', 'code6', 60))
-//    await Productos.addProducts(new Products('Producto 7', 'Descripción 7', 100, 'imagen7.jpg', 'code7', 70))
-//    await Productos.addProducts(new Products('Producto 8', 'Descripción 8', 100, 'imagen8.jpg', 'code8', 80))
-//    await Productos.addProducts(new Products('Producto 9', 'Descripción 9', 100, 'imagen9.jpg', 'code9', 90))
-//    await Productos.addProducts(new Products('Producto 10', 'Descripción 10', 100, 'imagen10.jpg', 'code10', 10))
-
-
-//     await Productos.updateProductById ({
-//         description: 'Esto esta maodifcado',
-//         price: 300,
-//         id:2
-//       })
-
-//     Productos.getProducts();
-
-//     await Productos.deleteProduct(3);
-
-//     await Productos.addProducts(new Products('Producto 1', 'Descripción 1', 100, 'imagen1.jpg', 'code1', 10));
-
-//     await Productos.addProducts(new Products('Producto 2', 'Descripción 3', 100, 'imagen3.jpg', 'code3', 10))
-
-
-//     Productos.getProducts()
-
-
-// // })
-// ();
-module.exports = ProductManager;
